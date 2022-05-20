04159a784f21e14356f47b9acf8619a10ae9e798
shashi <shashikumar3089@gmail.com>
2022-05-20 22:04:25 +0530

    removed sysout from main class

--- JUnit_Preparation/src/main/java/com/skoti/learning/JUnitPreparrationApplication.java
+++ JUnit_Preparation/src/main/java/com/skoti/learning/JUnitPreparrationApplication.java
@@ -8,7 +8,6 @@ public class JUnitPreparrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JUnitPreparrationApplication.class, args);
-		System.out.println("Welcome to Junit");
 	}
 
 }
